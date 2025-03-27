d501aafab71257d5e5eb086c83f19be1e6b795b3
Galent-git <lucidumingenium@hotmail.com>
2025-03-27 16:15:34 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
+import sys
+
+MIN_PY_VERSION = (3,6)
+
+def check_python_version():
+    if sys.version_info < MIN_PY_VERSION:
+        sys.stderr.write(f"Need Python {MIN_PY_VERSION[0]}.{MIN_PY_VERSION[1]} or later.\n")
+        sys.exit(1)
+
+CLEANED_SUBDIRS = [
+    "raw_stats",
+    "cleaned_data",
+    "qc_reports/fastqc_reports",
+    "logs",
+    "archive",
+    "qc_reports/fastqc_trimmed"
+]
 
